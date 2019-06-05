fa898190c52fbd4c4de628f23706d7b37cc95a51
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-05 08:55:12 +0800

    +

--- GB_Arch+Patterns_HW03_Brizhak/Program.cs
+++ GB_Arch+Patterns_HW03_Brizhak/Program.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Практическое задание к уроку 3 по курсу Архитектуры и шаблоны проектирования Брижак Андрей
+// Применить шаблон «Фабрика» для создания объектов, описывающих геометрические фигуры (круг,
+//квадрат, прямоугольник).
+
+
+
 namespace GB_Arch_Patterns_HW03_Brizhak
 {
     class Program
---
+++ GB_Arch+Patterns_HW03_Brizhak/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW03_Brizhak
+{
+   public abstract class Shape
+    {
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+}
