2dfb7b7f0c608fd1372e0eff6cc0b6f120550787
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-05 09:04:00 +0800

    +Square

---
+++ GB_Arch+Patterns_HW03_Brizhak/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW03_Brizhak
+{
+    class Square : Shape
+    {
+        public int Side { get; set; }
+
+        public override double CalculateArea() => System.Math.Pow(Side, 2);
+
+        public override double CalculatePerimeter() => Side*4;
+        
+    }
+}
