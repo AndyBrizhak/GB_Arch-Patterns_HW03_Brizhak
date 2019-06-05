c0049cf40130f5504a04b0b82ef342e97c90ccf8
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-05 11:09:49 +0800

    +LoadTypes()

---
+++ GB_Arch+Patterns_HW03_Brizhak/ShapeFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace GB_Arch_Patterns_HW03_Brizhak
+{
+   public class ShapeFactory
+    {
+        Dictionary<string, Type> shapes;
+
+        public ShapeFactory() => LoadTypes();
+
+        private void LoadTypes()
+        {
+            shapes = new Dictionary<string, Type>();
+
+            Type[] typesInThisAssembly = Assembly.GetExecutingAssembly().GetTypes();
+
+            //foreach (var item in typesInThisAssembly) Console.WriteLine($"---- {item.Name}");
+
+            foreach (Type type in typesInThisAssembly)
+            {
+                if (type.GetInterface(typeof(IShape).ToString()) != null)
+                {
+                    shapes.Add(type.Name, type);
+                }
+            }
+        }
+    }
+
+    
+}
