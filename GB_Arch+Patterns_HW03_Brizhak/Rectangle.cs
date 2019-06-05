ba6503b919031a12ef31d04081e57bb3c0464162
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-05 09:13:22 +0800

    +Rectangle

---
+++ GB_Arch+Patterns_HW03_Brizhak/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_HW03_Brizhak
+{
+    class Rectangle : Shape
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+        }
+
+        public override double CalculateArea() => Width * Height;
+        
+        public override double CalculatePerimeter() => (Width + Height)*2;
+        
+    }
+}
